1209d0c0e6b5960c8d3456156bf3de68454233b0
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2022-01-25 12:04:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Pawelko
+# Created on: Jan 2022
+# 2D array
+
+from random import randint
+
+
+def average(arr: list):
+    # variable
+    total = 0
+
+    # process/output
+    for row in arr:
+        for colm in row:
+            total += colm
+            print(colm, end=" ")
+        print("")
+
+    # output
+    print(f"\nAverage is {total/(len(arr)*len(arr[0]))}")
+
+
+def main():
+    # main function for array
+
+    # input
+    rows = input("How many rows: ")
+    rows = int(rows)
+    colm = input("How many columns: ")
+    colm = int(colm)
+
+    # create array
+    arr = [[randint(1, 50) for rand in range(colm)] for row in range(rows)]
+
+    # output/process/calling function
+    average(arr)
+
+    # done
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
